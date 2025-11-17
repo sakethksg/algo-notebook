afa6c1664f85c0b641ef431024aa6c7b5c7dd37b
sakethksg <ksgsaketh@gmail.com>
2025-11-17 18:29:13 +0530

    code added

---
+++ ChineseRemainder.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class ChineseRemainder {
+    int calculate(int size, int div[], int rem[]) {
+        int j, x = 1;
+        while (true) {
+            for (j = 0; j < size; j++) {
+                if (x % div[j] != rem[j])
+                    break;
+            }
+            if (j == size)
+                return x;
+            x++;
+        }
+    }
+}
+
+class ChineseRemainderTheorem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of divisors:");
+        int size = sc.nextInt();
+        int[] div = new int[size];
+        System.out.println("Enter divisors:");
+        for (int i = 0; i < size; i++) {
+            div[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter remainders:");
+        int[] rem = new int[size];
+        for (int i = 0; i < size; i++) {
+            rem[i] = sc.nextInt();
+        }
+
+        ChineseRemainder c = new ChineseRemainder();
+        System.out.println(c.calculate(size, div, rem));
+        sc.close();
+    }
+}
